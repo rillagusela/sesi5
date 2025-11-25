352a668f74271c8f2ef8e875d47a10c9e0bd3ab1
Rilla <rillagusela@gmail.com>
2025-11-26 00:40:58 +0700

    edit2

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-import { fileURLToPath } from 'node:url';
-import { GoogleGenAI as GoogleGenerativeAI} from '@google/genai';
+import { fileURLToPath } from 'url';
+import { GoogleGenAI } from '@google/genai';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+const GEMINI_MODEL = 'gemini-2.5-flash';
 
 app.use(cors());
 app.use(express.json());
@@ -23,21 +25,20 @@ app.listen(PORT, () => {
 });
 
 app.post('/api/chat', async (req, res) => {
-    const { history, message } = req.body;
+    const { conversation } = req.body;
     try {
-        if (!message) {
-            return res.status(400).json({ error: 'Message is required' });
-        }
+        if (!Array.isArray(conversation)) throw new Error('Message must be an array!');
 
-        const chat = model.startChat({
-            history: history || [],
+        const contents = conversation.map(({ role, text }) => ({
+            role,
+            parts: [{ text }]
+        }));
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents
         });
-
-        const result = await chat.sendMessage(message);
-        const response = await result.response;
-        const text = response.text();
-        res.status(200).json({ result: text });
+        res.status(200).json({ result: response.text });
     } catch (e) {
-        res.status(500).json({ error: `Something went wrong: ${e.message}` });
+        res.status(500).json({ error: e.message });
     }
 });
